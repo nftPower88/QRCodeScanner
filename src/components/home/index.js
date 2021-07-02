b501f3ff3bcbe3ff8c1d796ac770d3d9a59b79a2
Marko <seniortopdev2020@gmail.com>
2021-07-02 15:07:00 +0200

    add qr-scan control

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,23 +1,58 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { LanguageContext } from '../../localization/LanguageContainers';
 import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core/';
 import { useDispatch } from 'react-redux';
+import QrReader from 'react-qr-scanner';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: 'block',
+    textAlign: 'center',
+    justifyContent: 'center'
   },
+  control: {
+    display: 'flex',
+    textAlign: 'center',
+    justifyContent: 'center'
+  }
 }));
 
 const Home = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const { dictionary } = useContext(LanguageContext);
+  const [delay, setDelay] = useState(100);
+  const [result, setResult] = useState('No result');
+
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  }
+
+  const handleScan = (data) => {
+    setResult(data);
+  }
+
+  const handleError = (err) => {
+    console.log(err)
+  }
 
   return (
     <div className={classes.root}>
-    
+        <QrReader
+          delay={delay}
+          style={previewStyle}
+          onError={handleError}
+          onScan={handleScan}
+        />
+        <p>{result}</p>
+        <div className={classes.control}>
+          <Button>{dictionary.btnLabelCam}</Button>
+          <Button>{dictionary.btnLabelScreen}</Button>
+          <Button>{dictionary.btnLabelRecord}</Button>
+        </div>
     </div>     
   );
 }
